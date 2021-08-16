1b6a4e0deea109ccc18f30cd8c1582f3e926d59a
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-08-16 10:47:10 +0900

    Init

---
+++ habit-tracker/src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css";
+
+function app() {
+  const name = "chooble";
+  return (
+    <>
+      <h1>Hello!! {name}:)</h1>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default app;
